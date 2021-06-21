1af5d2e5381c1ffa1cd77a61d6cc839f821b19b4
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-06-21 19:45:36 +0700

    fix fillable for model

--- app/Models/PaymentHistory.php
+++ app/Models/PaymentHistory.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentHistory extends Model
 {
     use HasFactory;
+    protected $fillable = ['transaction_time','transaction_status','transaction_id','status_message','status_code','signature_key','payment_type','order_id','gross_amount','approval_code'];
 }
